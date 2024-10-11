8bd3b7491a5eea42fd82bee1d0e9dbac34b669db
Dan Treasure <beepboop3762@protonmail.com>
2024-10-11 13:54:51 -0700

    Adds a multiply method and changes data type to a floating point value, also changes the format function to handle the float

--- main.zig
+++ main.zig
@@ -1,8 +1,9 @@
 const std = @import("std");
+
 pub fn main() !void {
     const Value = struct {
-        data: u32,
-        pub fn init(data: u32) @This() {
+        data: f64,
+        pub fn init(data: f64) @This() {
             return .{
                 .data = data,
             };
@@ -16,10 +17,22 @@ pub fn main() !void {
         ) !void {
             _ = fmt;
             _ = options;
-            try writer.print("{}", .{self.data});
+            try writer.print("{d:.6}", .{self.data});
+        }
+
+        pub fn add(self: @This(), other: @This()) @This() {
+            return @This().init(self.data + other.data);
+        }
+
+        pub fn mul(self: @This(), other: @This()) @This() {
+            return @This().init(self.data * other.data);
         }
     };
 
-    const myValue = Value.init(10);
-    std.debug.print("myValue: {}\n", .{myValue});
+    const a = Value.init(4.0);
+    const b = Value.init(2.1);
+    const c = a.add(b);
+    const d = a.mul(b);
+    std.debug.print("c: {}\n", .{c});
+    std.debug.print("d: {}\n", .{d});
 }
