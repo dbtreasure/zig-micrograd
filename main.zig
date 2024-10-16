8f193fb965c8c97b0bd18987e932229edd086896
Dan Treasure <beepboop3762@protonmail.com>
2024-10-16 19:54:50 -0400

    adds an operator attribute to the value struct

--- main.zig
+++ main.zig
@@ -5,18 +5,21 @@ pub fn main() !void {
         data: f64,
         prev: std.ArrayList(@This()),
         allocator: std.mem.Allocator,
+        operator: ?[]const u8,
 
-        pub fn init(allocator: std.mem.Allocator, data: f64, _children: ?std.ArrayList(@This())) !@This() {
+        pub fn init(allocator: std.mem.Allocator, data: f64, _children: ?std.ArrayList(@This()), operator: ?[]const u8) !@This() {
             var prevList = std.ArrayList(@This()).init(allocator);
             if (_children) |children| {
                 for (children.items) |*child| { // Access items as a property, not a function
                     try prevList.append(child.*); // Dereference the pointer
                 }
             }
+
             return .{
                 .data = data,
                 .prev = prevList,
                 .allocator = allocator,
+                .operator = operator,
             };
         }
 
@@ -35,22 +38,28 @@ pub fn main() !void {
             var children = std.ArrayList(@This()).init(self.allocator);
             try children.append(self);
             try children.append(other);
-            return @This().init(self.allocator, self.data + other.data, children);
+            return @This().init(self.allocator, self.data + other.data, children, "+");
         }
 
         pub fn mul(self: @This(), other: @This()) !@This() {
             var children = std.ArrayList(@This()).init(self.allocator);
             try children.append(self);
             try children.append(other);
-            return @This().init(self.allocator, self.data * other.data, children);
+            return @This().init(self.allocator, self.data * other.data, children, "*");
         }
     };
 
     const allocator = std.heap.page_allocator;
-    const a = try Value.init(allocator, 4.0, null);
-    const b = try Value.init(allocator, 2.0, null);
-    const c = try Value.init(allocator, 3.0, null);
+    const a = try Value.init(allocator, 4.0, null, null);
+    const b = try Value.init(allocator, 2.0, null, null);
+    const c = try Value.init(allocator, 3.0, null, null);
     const d = try (try a.mul(b)).add(c);
 
     std.debug.print("d: {}\n", .{d});
+
+    // debug print d's children
+    std.debug.print("d's children: {any}\n", .{d.prev.items});
+    if (d.operator) |op| {
+        std.debug.print("d's operator: {s}\n", .{op});
+    }
 }
